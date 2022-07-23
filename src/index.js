0121d0100451cedaa05e9af2614245d3a84a1c1b
YaroslavOnofriichuk <YaroslavOnofriichuk@ukr.net>
2022-07-23 13:12:23 +0300

    add styles

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
 import "./styles/main.scss";
+
+const burgerButton = document.querySelector("[data-modal-open]");
+const modal = document.querySelector("[data-modal]");
+const closeButton = document.querySelector("[data-modal-close]");
+
+function openModal() {
+  modal.classList.toggle("is-hidden");
+  document.body.style.overflow = "hidden";
+}
+
+function closeModal() {
+  modal.classList.toggle("is-hidden");
+  document.body.style.overflow = "";
+}
+burgerButton.addEventListener("click", openModal);
+
+closeButton.addEventListener("click", closeModal);
